47d64ecc27182b36cfa6c39c27732b2de3bf480d
Romain OEIL <romain.oeil@epitech.eu>
2024-06-26 16:35:53 +0200

    assets for fight scene

---
+++ EIP/Assets/FightScene/Scripts/StartupFight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class StartupFight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
