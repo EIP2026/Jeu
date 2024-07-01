62b584458a96a4eec0e1fa858c2eb4d350954e96
NoahCherel <noah.cherel@epitech.eu>
2024-07-01 17:52:48 +0200

    [ADD] button in market now do something

--- EIP/Assets/Scripts/HandleButtonOutline.cs
+++ EIP/Assets/Scripts/HandleButtonOutline.cs
@@ -1,17 +1,13 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonOutlineHandler : MonoBehaviour
 {
     public Outline outline;
-    private Text buttonText;
-
+    public Image childImage;
     void Start()
     {
-        Debug.Log("HEY");
-        // Print the button name
-        Debug.Log("Button name: " + gameObject.name);
-
         // Get the Outline component
         outline = GetComponent<Outline>();
         // Print an error if the Outline component is not found
@@ -20,15 +16,7 @@ public class ButtonOutlineHandler : MonoBehaviour
             Debug.LogError("Outline component not found");
             return;
         }
-
-        // Get the Text component
-        buttonText = GetComponentInChildren<Text>();
-        // Print an error if the Text component is not found
-        if (buttonText == null)
-        {
-            Debug.LogError("Text component not found");
-            return;
-        }
+        childImage = GetComponentInChildren<Image>();
     }
 
     public void OnButtonClick()
@@ -36,7 +24,8 @@ public class ButtonOutlineHandler : MonoBehaviour
         // Enable the outline and set it to only show at the bottom
         outline.effectColor = Color.white;
         outline.effectDistance = new Vector2(0, -5);
-        // Make the text bold
-        buttonText.fontStyle = FontStyle.Bold;
+
+        // Reveal the child image
+        childImage.enabled = true;
     }
 }
