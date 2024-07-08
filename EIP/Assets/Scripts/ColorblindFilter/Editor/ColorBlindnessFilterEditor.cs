1aa3cec9d0357773d01e855b4fe4b6d4ffdb7930
Marc-Olivier KOBAN <marc-olivier.koban@epitech.eu>
2024-07-08 13:47:54 +0200

    add plugin color blind

---
+++ EIP/Assets/Scripts/ColorblindFilter/Editor/ColorBlindnessFilterEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+namespace ColorblindFilter.Editor
+{
+    [CustomEditor(typeof(global::ColorblindFilter.Scripts.ColorblindFilter))]
+    public class ColorBlindnessFilterEditor : UnityEditor.Editor
+    {
+        private global::ColorblindFilter.Scripts.ColorblindFilter _colorblindFilter;
+
+        private SerializedProperty _blindnessType;
+        private SerializedProperty _useFilter;
+
+        private void OnEnable()
+        {
+            _colorblindFilter = target as global::ColorblindFilter.Scripts.ColorblindFilter;
+            _blindnessType = serializedObject.FindProperty("_blindnessType");
+            _useFilter = serializedObject.FindProperty("_useFilter");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(_useFilter);
+            EditorGUILayout.PropertyField(_blindnessType);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
